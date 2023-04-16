3ee382f5899309f00ba329eba5e2c71f3af63b07
Murka124 <67805801+Murka124@users.noreply.github.com>
2023-04-16 17:28:11 +0300

    добавил функции шифрования/расшифрования
    в целом немного почищен код, вынесены методы для шифрования/дешифрования сообщения в отдельные функции и добавлена двусторонняя проверка шифрования (выполняется сразу после подключения клиента к серверу)

--- client.js
+++ client.js
@@ -1,9 +1,28 @@
 const WebScoket = require('ws')
 const crypto = require('node:crypto')
+const message = "далбаю"
+let key //оставляем как есть, ключ будет определён при его получении
 
+// Функции ========
+    // для расшифровки сообщения
+    function decryptMessage(content) {
+        iv = Buffer.from(content.iv,'hex')
+        const decipher = crypto.createDecipheriv('aes-256-cbc',key,iv)
+        const decryptedBuffer = Buffer.concat([decipher.update(Buffer.from(content.data, 'hex')), decipher.final()]);
+        return decryptedBuffer.toString('utf8');
+    }
+
+    // для шифрования сообщения
+    function encryptMessage(content) {
+        const iv = crypto.randomBytes(16)
+        const cipher = crypto.createCipheriv('aes-256-cbc',key,iv)
+        const encryptedBuffer = Buffer.concat([cipher.update(content, 'utf8'), cipher.final()]);
+        const encryptedMessage = encryptedBuffer.toString('hex');
+        return(JSON.stringify({type:'msg',data: encryptedMessage, iv:iv.toString('hex')}))
+    }
+
+// Подключаемся к серверу
 const srv = new WebScoket('ws://localhost:8080')
-
-let key
 
 srv.onopen = event => {
     console.log('Подключено.')
@@ -11,18 +30,18 @@ srv.onopen = event => {
 
 srv.onmessage = event => {
     const msg = JSON.parse(event.data)
-    //Выполняем проверку на публичный ключ
+
+    // Выполняем проверку на ключ
     if (msg.type == 'msgKey') {
         console.log(msg)
         key = Buffer.from(msg.data,'hex')
-        // cipher = crypto.createCipheriv('aes-256-ccm',key,iv)
         console.log('Ключ получен.',key)
-    } else {
-        iv = Buffer.from(msg.iv,'hex')
-        const decipher = crypto.createDecipheriv('aes-256-cbc',key,iv)
-        const decryptedBuffer = Buffer.concat([decipher.update(Buffer.from(msg.data, 'hex')), decipher.final()]);
-        const decryptedMessage = decryptedBuffer.toString('utf8');
-        console.log('[Server]',decryptedMessage)
+    }
+    
+    // Иначе парсим как обычное сообщение
+    else {
+        console.log('[Server]',decryptMessage(msg))
+        srv.send(encryptMessage(message))
     }
 }
 
--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const WebScoket = require('ws')
+const WebSocket = require('ws')
 const uuid = require('uuid').v4
 const crypto = require('node:crypto')
 const fs = require('node:fs')
 
-// const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {modulusLength: 4096,publicKeyEncoding: {type: 'spki',format: 'pem'},privateKeyEncoding: {type: 'pkcs8',format: 'pem'}});
+// Функции ========
+    // для расшифровки сообщения
+    function decryptMessage(content) {
+        iv = Buffer.from(content.iv,'hex')
+        const decipher = crypto.createDecipheriv('aes-256-cbc',key,iv)
+        const decryptedBuffer = Buffer.concat([decipher.update(Buffer.from(content.data, 'hex')), decipher.final()]);
+        return decryptedBuffer.toString('utf8');
+    }
 
+    // для шифрования сообщения
+    function encryptMessage(content) {
+        const iv = crypto.randomBytes(16)
+        const cipher = crypto.createCipheriv('aes-256-cbc',key,iv)
+        const encryptedBuffer = Buffer.concat([cipher.update(content, 'utf8'), cipher.final()]);
+        const encryptedMessage = encryptedBuffer.toString('hex');
+        return(JSON.stringify({type:'msg',data: encryptedMessage, iv:iv.toString('hex')}))
+    }
+
+// Генерируем ключ
 const key = crypto.randomBytes(32)
 console.log('Ключ:',key.toString('hex'))
 
+// Конфигурируем сервер
 const config = { port: 8080 }
 const port = config.port
-
-const wss = new WebScoket.Server({ port: port })
+const wss = new WebSocket.Server({ port: port })
 
 wss.on('listening', () => {
     console.log('Прослушиваем порт', port)
@@ -21,22 +38,21 @@ let clients = JSON.parse('{}')
 wss.on('connection', ws => {
     let id = uuid()
     clients[id] = ws
-    ws.send(JSON.stringify({type:'msgKey',data: key.toString('hex')})) // Крайне небезопасный способ передачи ключа. Переделать. Когда нибудь...
+    ws.send(JSON.stringify({type:'msgKey',data: key.toString('hex')}))
+    // Строка выше - крайне небезопасный способ передачи ключа.
+    // Для неё и была создана отдельная ветка, чтобы не портить основную.
     console.log(id, 'присоединился.')
     
-    // Отправляем всякую хуйню
-
+    // Отправляем всякую фигню для проверки шифрования
     const message = "HELLO WORLD!!!!"
-    const iv = crypto.randomBytes(16)
-    cipher = crypto.createCipheriv('aes-256-cbc',key,iv)
-    const encryptedBuffer = Buffer.concat([cipher.update(message, 'utf8'), cipher.final()]);
-    const encryptedMessage = encryptedBuffer.toString('hex');
-    ws.send(JSON.stringify({type:'msg',data: encryptedMessage, iv:iv.toString('hex')}))
+    ws.send(encryptMessage(message, 0))
 
-    ws.on('message', msg => {
-        // console.log('[',id,']',msg)
-        // try{const decryptData = crypto.privateDecrypt({key: privateKey,padding: crypto.constants.RSA_PKCS1_OAEP_PADDING},Buffer.from(JSON.parse(msg).data,'base64'));console.log('[',id,']',decryptData.toString())}catch(err){console.log('Не удалось расшифровать:',err)}});let cryptData = crypto.publicEncrypt({key: publicKey,padding: crypto.constants.RSA_PKCS1_OAEP_PADDING},Buffer.from('HELLO WORLD!!!','utf-8'));ws.send(JSON.stringify({type:'cryptData',data: cryptData.toString('base64')}
+    ws.on('message', event => {
+        const msg = JSON.parse(event)
 
+        if (msg.type == "msg") {
+            console.log('[Client]',decryptMessage(msg))
+        }
     })
 
     ws.on('close', function () {
