9945f0e21162d61ec7ad56c9df2c476e9c5a5e46
Котя <71443736+kotisoff@users.noreply.github.com>
2023-04-16 14:43:28 +0300

    Try to create new encrypt system with crypto.
    Я устал.

--- client.js
+++ client.js
@@ -3,21 +3,25 @@ const crypto = require('node:crypto')
 
 const srv = new WebScoket('ws://localhost:8080')
 
-let publicKey
+let key, iv, cipher, decipher
 
 srv.onopen = event => {
     console.log('Подключено.')
 }
 
-srv.onmessage = msg =>{
-    // console.log('[SERVER]',msg)
-    if(msg.type=='pubKey'){
-        publicKey=JSON.parse(msg).data
-        console.log('Публичный ключ создан.')
+srv.onmessage = event => {
+    const msg = JSON.parse(event.data)
+    //Выполняем проверку на публичный ключ
+    if (msg.type == 'msgKey') {
+        key = msg.data
+        cipher = crypto.createCipheriv('aes-256-ccm',key,iv)
+        decipher = crypto.createDecipheriv('aes-256-ccm',key,iv)
+        console.log('Ключ получен.')
+    } else {
+        // try {const decryptData = crypto.publicDecrypt({key: publicKey,padding: crypto.constants.RSA_PKCS1_OAEP_PADDING},Buffer.from(JSON.parse(msg.data).data, 'base64'));console.log('[Server]', decryptData.toString())} catch (err) {console.log('Не удалось расшифровать:', err)}
     }
-
 }
 
 srv.onclose = event => {
-    console.log('СЕРВЕР ЗАКРЫТ',JSON.stringify(event))
+    console.log('СЕРВЕР ЗАКРЫТ', JSON.stringify(event))
 }
--- server.js
+++ server.js
@@ -3,18 +3,13 @@ const uuid = require('uuid').v4
 const crypto = require('node:crypto')
 const fs = require('node:fs')
 
-const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 4096,
-    publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem'
-    },
-    privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem'
-    }
-});
+// const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {modulusLength: 4096,publicKeyEncoding: {type: 'spki',format: 'pem'},privateKeyEncoding: {type: 'pkcs8',format: 'pem'}});
 
+const key = crypto.randomBytes(32).toString('hex')
+const cipher = crypto.createCipheriv('aes-256-ccm',key)
+const decipher = crypto.createDecipheriv('aes-256-ccm',key)
+let iv
+console.log('Ключ:',key)
 
 const config = { port: 8080 }
 const port = config.port
@@ -29,27 +24,16 @@ let clients = JSON.parse('{}'), uuids = []
 wss.on('connection', ws => {
     let id = uuid()
     clients[id] = ws
-    ws.send(JSON.stringify({type:'pubKey',data: publicKey}))
+    iv = crypto.randomBytes(16)
+    ws.send(JSON.stringify({type:'msgKey',data: key, iv: iv})) // Крайне небезопасный способ передачи ключа. Переделать. Когда нибудь...
     console.log(id, 'присоединился.')
+
     ws.on('message', msg => {
-        console.log('[',id,']',msg)
-        try{
-            const decryptData = crypto.privateDecrypt(
-                {
-                    key: privateKey,
-                    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
-                },
-                Buffer.from(JSON.parse(msg).data,'base64')
-            );
-            console.log('[',id,']',decryptData.toString())
-        }catch(err){console.log('Не удалось расшифровать:',err)}
+        // console.log('[',id,']',msg)
+        // try{const decryptData = crypto.privateDecrypt({key: privateKey,padding: crypto.constants.RSA_PKCS1_OAEP_PADDING},Buffer.from(JSON.parse(msg).data,'base64'));console.log('[',id,']',decryptData.toString())}catch(err){console.log('Не удалось расшифровать:',err)}});let cryptData = crypto.publicEncrypt({key: publicKey,padding: crypto.constants.RSA_PKCS1_OAEP_PADDING},Buffer.from('HELLO WORLD!!!','utf-8'));ws.send(JSON.stringify({type:'cryptData',data: cryptData.toString('base64')}
+
     })
-    let cryptData = crypto.publicEncrypt({
-        key: publicKey,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
-    },
-    Buffer.from('HELLO WORLD!!!','utf-8'))
-    ws.send(JSON.stringify({type:'cryptData',data: cryptData.toString('base64')}))
+
     ws.on('close', function () {
         delete clients[id]
         console.log(id, 'отключился.')
