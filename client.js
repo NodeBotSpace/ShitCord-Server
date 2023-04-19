4be7a72091d7d652f81c1c2d7af9bd4d76ce0640
Murka124 <67805801+Murka124@users.noreply.github.com>
2023-04-19 16:27:54 +0300

    зачем-то подобавлял ";" и обновил реадмё

--- client.js
+++ client.js
@@ -1,33 +1,33 @@
-const WebScoket = require('ws')
-const msgcrypt = require('./msgcrypt.js')
-const message = "далбаю"
-let key //оставляем как есть, ключ будет определён при его получении
+const WebScoket = require('ws');
+const msgcrypt = require('./msgcrypt.js');
+const message = "далбаю";
+let key; //оставляем как есть, ключ будет определён при его получении
 
 // Подключаемся к серверу
-const srv = new WebScoket('ws://localhost:8080')
+const srv = new WebScoket('ws://localhost:8080');
 
 srv.onopen = event => {
-    console.log('Connected.')
+    console.log('Connected.');
 }
 
 srv.onmessage = event => {
-    const msg = JSON.parse(event.data)
+    const msg = JSON.parse(event.data);
 
     // Выполняем проверку на ключ
     if (msg.type == 'msgKey') {
-        console.log('Server message:',msg)
-        key = Buffer.from(msg.data,'hex')
-        console.log('Key received:',key.toString('hex'))
+        console.log('Server message:', msg);
+        key = Buffer.from(msg.data, 'hex');
+        console.log('Key received:', key.toString('hex'));
     }
-    
+
     // Иначе парсим как обычное сообщение
     else {
-        const decryptedMessage = msgcrypt.decryptMessage(msg,key)
-        console.log('[Server]',decryptedMessage)
-        srv.send(msgcrypt.encryptMessage(message,"msg",key))
+        const decryptedMessage = msgcrypt.decryptMessage(msg, key);
+        console.log('[Server]', decryptedMessage);
+        srv.send(msgcrypt.encryptMessage(message, "msg", key));
     }
 }
 
 srv.onclose = event => {
-    console.log('СЕРВЕР ЗАКРЫТ', JSON.stringify(event))
+    console.log('СЕРВЕР ЗАКРЫТ', JSON.stringify(event));
 }
--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-const WebSocket = require('ws')
-const uuid = require('uuid').v4
-const crypto = require('node:crypto')
-const msgcrypt = require('./msgcrypt.js')
-const fs = require('node:fs')
+const WebSocket = require('ws');
+const uuid = require('uuid').v4;
+const crypto = require('node:crypto');
+const msgcrypt = require('./msgcrypt.js');
+const fs = require('node:fs');
 
 // Генерируем ключ
-const key = crypto.randomBytes(32)
-console.log('Ключ:',key.toString('hex'))
+const key = crypto.randomBytes(32);
+console.log('Ключ:', key.toString('hex'));
 
 // Конфигурируем сервер
-const config = { port: 8080 }
-const port = config.port
-const wss = new WebSocket.Server({ port: port })
+const config = { port: 8080 };
+const port = config.port;
+const wss = new WebSocket.Server({ port: port });
 
 wss.on('listening', () => {
-    console.log('Прослушиваем порт', port)
+    console.log('Прослушиваем порт', port);
 })
 
-let clients = JSON.parse('{}')
+let clients = JSON.parse('{}');
 wss.on('connection', ws => {
-    let id = uuid()
-    clients[id] = ws
-    ws.send(JSON.stringify({type:'msgKey',data: key.toString('hex')}))
+    let id = uuid();
+    clients[id] = ws;
+    ws.send(JSON.stringify({ type: 'msgKey', data: key.toString('hex') }));
     // Строка выше - крайне небезопасный способ передачи ключа.
     // Для неё и была создана отдельная ветка, чтобы не портить основную.
-    console.log(id, 'присоединился.')
-    ws.send(msgcrypt.encryptMessage('Your UUID: '+id,'msg',key))
-    
+    console.log(id, 'присоединился.');
+    ws.send(msgcrypt.encryptMessage('Your UUID: ' + id, 'msg', key));
+
     // Отправляем всякую фигню для проверки шифрования
-    const message = "HELLO WORLD!!!!"
-    ws.send(msgcrypt.encryptMessage(message,"msg",key))
+    const message = "HELLO WORLD!!!!";
+    ws.send(msgcrypt.encryptMessage(message, "msg", key));
 
     ws.on('message', event => {
-        const msg = JSON.parse(event)
+        const msg = JSON.parse(event);
 
         if (msg.type == "msg") {
-            console.log('[Client]',msgcrypt.decryptMessage(msg,key))
-            ws.send(msgcrypt.encryptMessage('client msg: '+msgcrypt.decryptMessage(msg,key),'msg',key))
+            console.log('[Client]', msgcrypt.decryptMessage(msg, key));
+            ws.send(msgcrypt.encryptMessage('client msg: ' + msgcrypt.decryptMessage(msg, key), 'msg', key));
         }
     })
 
     ws.on('close', function () {
-        delete clients[id]
-        console.log(id, 'отключился.')
+        delete clients[id];
+        console.log(id, 'отключился.');
     })
 })
