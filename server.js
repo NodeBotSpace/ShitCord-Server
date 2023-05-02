1feb9c2fe0b39804d4d8eeec6c6a9cf0117d6eb4
Котя <71443736+kotisoff@users.noreply.github.com>
2023-05-03 01:58:49 +0300

    Что то на ночь сделал. Чуть подвигал шифрование.

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const fs = require('node:fs');
 const key = crypto.randomBytes(32);
 console.log('Ключ:', key.toString('hex'));
 
+// Получаем список пользователей
+
+if(fs.existsSync('./serverdata')) fs.mkdirSync('./serverdata')
+let users
+try{
+    console.log('importing users')
+    users=require('./serverdata/usrDb.json')
+}catch{
+    fs.writeFileSync('./serverdata/usrDb.json','{}')
+    users=JSON.parse('{}')
+}
+
 // Конфигурируем сервер
 const config = { port: 8080 };
 const port = config.port;
@@ -21,23 +33,26 @@ let clients = JSON.parse('{}');
 wss.on('connection', ws => {
     let id = uuid();
     clients[id] = ws;
-    ws.send(JSON.stringify({ type: 'msgKey', data: key.toString('hex') }));
+    ws.send(JSON.stringify({ type: 'key', data: key.toString('hex') }));
     // Строка выше - крайне небезопасный способ передачи ключа.
     // Для неё и была создана отдельная ветка, чтобы не портить основную.
     console.log(id, 'присоединился.');
+    ws.send(msgcrypt.encryptMessage(id,'uuid',key))
     ws.send(msgcrypt.encryptMessage('Your UUID: ' + id, 'msg', key));
-
-    // Отправляем всякую фигню для проверки шифрования
-    const message = "HELLO WORLD!!!!";
-    ws.send(msgcrypt.encryptMessage(message, "msg", key));
 
     ws.on('message', event => {
         const msg = JSON.parse(event);
 
+        if(msg.type=="auth"){
+            let decryptedData=[]
+            decryptedData.push(msgcrypt.decryptMessage(msg.login),msgcrypt.decryptMessage(msg.password))
+            if(users.find('')){}
+        }
+
         if (msg.type == "msg") {
-            let decryptedMessage = msgcrypt.decryptMessage(msg,key)
+            let decryptedMessage = msgcrypt.decryptMessage(msg.data,msg.iv,key)
             console.log('[Client]',decryptedMessage)
-            ws.send(msgcrypt.encryptMessage('client msg: '+msgcrypt.decryptMessage(msg,key),'msg',key))
+            ws.send(msgcrypt.encryptMessage('client msg: '+msgcrypt.decryptMessage(msg.data,msg.iv,key),'msg',key))
             if(decryptedMessage=="/kickme"){ws.close(1000,"Причина посылания нахуй: Еблан")}
         }
     })
